ca707f4fb62f381ce14e766c74d4d85ea38d2d9d
moisesAlc <msalcmd@gmail.com>
2020-12-11 01:27:05 -0300

    respondendo 'Olá + nome' em :8080/ola

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@GetMapping("/ola")
+	public String ola(@RequestParam(value = "nome", defaultValue = "Mundo") String nome) {
+		return String.format("Olá %s!", nome);
+	}
 }
